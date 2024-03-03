75777df80b4f03aeafc0d16b4943a74911a77e3f
chrisreber <christopherreber@gmail.com>
2024-03-03 15:10:12 -0500

    Added function names, please fill in your name next to "Contributor"  for sections you will be working on

--- RsaAlgorithmProject.s
+++ RsaAlgorithmProject.s
@@ -1,9 +1,40 @@
 .text
 .global main
+
+gcd:
+  # Contributor: 
+  # computes and returns the greatest common divisor of two integers
+
+pow:
+  # Contributor:
+  # performs exponentiation of two integers
+
+modulo:
+  # Contributor:
+  # function to perform modulo operation
+
+cpubexp:
+  # Contributor:
+  # generates the public key
+
+cprivexp:
+  # Contributor:
+  # generates the private key
+
+encrypt:
+  # Contributor:
+  # encrypts an input string
+
+decrypt:
+  # Contributor:
+  # decrypts a .txt file
+
 main:
   # push the stack
   SUB sp, sp, #4
   STR lr, [sp, #0]
+
+  # main code starts here
 
   # pop the stack and return
   LDR lr, [sp, #0]
