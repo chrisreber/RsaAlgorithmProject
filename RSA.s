73539585f348749f5dfc13a130ba15564e78bbd4
Henry <sp2481527f7c@rpi-buster-armhf>
2024-03-24 04:44:49 +0000

    Completed Steps 2a-2d, fixed formatting using style guidelines

--- RSA.s
+++ RSA.s
@@ -2,14 +2,111 @@
 .global main
 
 main:
-  # push the stack
-  SUB sp, sp, #4
-  STR lr, [sp, #0]
+    # Push the stack
+    SUB sp, sp, #4
+    STR lr, [sp, #0]
 
-  # main code starts here
+    # Prompt for and receive input for p and q
+    # Contributor: Andrea Henry
+  
+    # While p is not prime, get user input
+    # r4 - Desired output (1=True for prime)
+    # r5 - Primality of p (initialize as 0)
+    MOV r4, #0
+    MOV r5, #0
+    StartGetPLoop:
+        # If p is prime, end loop
+        CMP r4, r5
+        BNE EndGetPLoop
+            # Print prompt
+            LDR r0, =pInputPrompt
+            BL printf
 
+            # Get input
+            LDR r0, =pInput
+            LDR r1, =p
+            BL scanf
+
+            # Check primality of p
+            LDR r0, =p
+            LDR r0, [r0]
+            BL isPrime 
+
+            # If p is prime, end loop
+            MOV r5, r0
+            CMP r4, r5
+            BNE EndGetPLoop
+                # Print error message
+                LDR r0, =pqErrorMsg
+                BL printf
+                B StartGetPLoop
+      
+    EndGetPLoop:            
+        # While q is not prime, get user input
+        # r4 - Desired output (1=True for prime)
+        # r5 - Primality of q (initialize as 0)
+        MOV r4, #0
+        MOV r5, #0
+        StartGetQLoop:
+        # If q is prime, end loop
+            CMP r4, r5
+            BNE EndGetQLoop
+                # Print prompt
+                LDR r0, =qInputPrompt
+                BL printf
+
+                # Get input
+                LDR r0, =pInput
+                LDR r1, =q
+                BL scanf
+
+                # Check primality of q
+                LDR r0, =q
+                LDR r0, [r0]
+                BL isPrime 
+
+                # If q is prime, end loop
+                MOV r5, r0
+                CMP r4, r5
+                BNE EndGetQLoop
+                    # Print error message
+                    LDR r0, =pqErrorMsg
+                    BL printf
+                    B StartGetQLoop
+      
+    EndGetQLoop:           
+        # Calculate modulus n for public and private keys
+        # Contributor: Andrea Henry
+
+        MOV r5, r1  // Save q
+        MUL r6, r4, r5  // r0 = n = pq
+
+        # Calculate totient, T = (p-1)(q-1)
+        # Contributor: Andrea Henry
+
+        SUB r4, r4, #1
+        SUB r5, r5, #1
+        MUL r7, r4, r5
+     
+  # For future operations, T is in r7. Other regs can be overwritten
+  
+  
   # pop the stack and return
   LDR lr, [sp, #0]
   ADD sp, sp, #4
   MOV pc, lr
+
 .data
+    # Variables needed for getting p and q from user
+    # Prompts
+    pInputPrompt: .asciz "Enter a positive integer less than 50 (p): "
+    qInputPrompt: .asciz "Enter a postive integer less than 50 (q): "
+    # Formatting
+    pInput: .asciz "%d"
+    qInput: .asciz "%d"
+    # User-input values
+    p: .word 0
+    q: .word 0
+    # Error message
+    pqErrorMsg: .asciz "Invalid entry.\n"
+
--- RSALib.s
+++ RSALib.s
@@ -2,6 +2,7 @@
 .global gcd
 .global pow
 .global modulo
+.global isPrime
 .global cpubexp
 .global cprivexp
 .global encrypt
@@ -9,145 +10,166 @@
 
 
 gcd:
-  # Contributor: 
-  # computes and returns the greatest common divisor of two integers
+    # Contributor: 
+    # computes and returns the greatest common divisor of two integers
 
-pow:
-  # Contributor: Andrea
-  # Purpose: Performs exponentiation of two integers
-  # Inputs: r0 - Number to raise to a power (num)
-  #	    r1 - Power to raise to (pow)
-  # Output: r0 - Product
 
-  # Internal Note: number and pow should be nonnegative integers 
-  # (error checking should happen before passing to this function)
-
-  # Push link register to stack, store all reg vals
-  # so original vals are restored when function ends
-  SUB sp, sp, #12
-  STR lr, [sp, #0]
-  STR r4, [sp, #4]
-  STR r5, [sp, #8]
-   
-  # Initialize counter and product
-  MOV r4, #0
-  MOV r5, #1
-
-  StartMultLoop:
-
-    # If counter >= loop max, end loop
-    CMP r4, r1
-    BGE EndMultLoop
-
-    # Multiply num by itself and increment counter
-    MUL r5, r5, r0
-    ADD r4, r4, #1
-
-    B StartMultLoop
-
-  EndMultLoop:
-  
-  # Restore original values
-  LDR lr, [sp, #0]
-  LDR r4, [sp, #4]
-  LDR r5, [sp, #8]
-  ADD sp, sp, #12
-
-  MOV pc, lr
-
-# End pow
-
-modulo:
-  # Contributor: Andrea
-  # Purpose: Function to perform modulo operation
-  # Inputs: r0 - Dividend (num)
-  # 	    r1 - Divisor (mod)
-  # Output: r0 - Remainder
-
-  # Internal Note: num and mod should be nonnegative integers 
-  # (error checking should happen before passing to this function)
-  
-  # Push link register to stack, store all reg vals
-  # so original vals are restored when function ends
-  SUB sp, sp, #12
-  STR lr, [sp, #0]
-  STR r4, [sp, #4]
-  STR r5, [sp, #8]
-    
-  MOV r4, r0      // Store num in r4 (r0 gets overwritten)
-  BL __aeabi_idiv // r0 = num//mod (integer division)
-  MUL r5, r0, r1  // r5 = (num//mod) * mod
-  SUB r0, r4, r5  // r0 = remainder
-  
-  # Restore original values
-  LDR lr, [sp, #0]
-  LDR r4, [sp, #4]
-  LDR r5, [sp, #8]
-  ADD sp, sp, #12
-
-  MOV pc, lr 
-
-# End modulo
+# Function: pow
+# Contributor: Andrea
+# Purpose: Performs exponentiation of two integers
+# Inputs: r0 - Number to raise to a power (num)
+#         r1 - Power to raise to (pow)
+# Output: r0 - Product
 
 .text
-isPrime:
-  # Contributor: Andrea Henry
-  # Purpose: Determines whether the input number is prime
-  # Inputs: r0 - Number to check primality for (num)
-  # Output: r0 - 0 if number is not prime, 1 if number is prime
+pow:
+    # Push lr to stack, store original preserved reg vals
+    SUB sp, sp, #12
+    STR lr, [sp, #0]
+    STR r4, [sp, #4]
+    STR r5, [sp, #8]
+   
+    # Initialize counter and product
+    MOV r4, #0
+    MOV r5, #1
+    StartMultLoop:
+        # If counter >= pow, end loop
+        CMP r4, r1
+        BGE EndMultLoop
+            # Multiply num by itself
+            MUL r5, r5, r0
 
-  SUB sp, sp, #20
-  STR lr, [sp, #0]
-  STR r4, [sp, #4]
-  STR r5, [sp, #8]
-  STR r6, [sp, #12]
-  STR r7, [sp, #16]
+            # Increment counter
+            ADD r4, r4, #1
+            B StartMultLoop
+   
+    EndMultLoop:
+        # Pop stack and exit
+        LDR lr, [sp, #0]
+        LDR r4, [sp, #4]
+        LDR r5, [sp, #8]
+        ADD sp, sp, #12
+        MOV pc, lr
 
-  # Save base array and size
-  LDR r4, =primeArray
-  LDR r5, =primeArraySize
-  LDR r5, [r5]
+# End OF pow
 
-  MOV r7, r0  // Store num in preserved register
+# Function: modulo
+# Contributor: Andrea
+# Purpose: Function to perform modulo operation
+# Inputs: r0 - Dividend (num)
+#         r1 - Divisor (mod)
+# Output: r0 - Remainder
+
+.text
+modulo:
+    # Push lr to stack, store original preserved reg vals
+    SUB sp, sp, #12
+    STR lr, [sp, #0]
+    STR r4, [sp, #4]
+    STR r5, [sp, #8]
+    
+    MOV r4, r0      // Store num in r4
+
+    # Calculate modulo
+    BL __aeabi_idiv // r0 = num//mod (integer division)
+    MUL r5, r0, r1  // r5 = (num//mod) * mod
+    SUB r0, r4, r5  // r0 = remainder
   
-  # Initialize loop for checking primes
-  # r4 - Array base
-  # r5 - End loop index
-  # r6 - Counter
-
-  MOV r6, #0
-  startCheckLoop:
-
-    # If counter >= array size, end loop
-    CMP r6, r5
-    BGE endCheckLoop
-    
-    # Set up inputs for mod: r0 = num, r1 = prime
-    MOV r0, r7
-    ADD r1, r4, r6, LSL #2
-    LDR r1, [r1, #0]
-    BL mod
-
-    # num mod prime == 0 --> return 0 (false, not prime)
-    CMP r0, #0
-    BEQ return
-    ADD r6, r6, #1  // Otherwise, increment counter
-    B startCheckLoop
-
-  endCheckLoop:
-
-  MOV r0, #1  // No number was divisible, set r0 = 1 (true, prime)
-    
-  return:
-    # Restore all original values
+    # Pop stack and exit
     LDR lr, [sp, #0]
     LDR r4, [sp, #4]
     LDR r5, [sp, #8]
-    LDR r6, [sp, #12]
-    LDR r7, [sp, #16]
-    ADD sp, sp, #20
+    ADD sp, sp, #12
+    MOV pc, lr 
 
-    MOV pc, lr
+# END OF modulo
+
+# Function: isPrime
+# Contributor: Andrea Henry
+# Purpose: Determines whether the input number is prime
+# Inputs: r0 - Number to check primality for (num)
+# Output: r0 - 0 if number is not prime, 1 if number is prime
+
+.text
+isPrime:  
+    # Push lr to stack, store original preserved reg vals
+    SUB sp, sp, #20
+    STR lr, [sp, #0]
+    STR r4, [sp, #4]
+    STR r5, [sp, #8]
+    STR r6, [sp, #12]
+    STR r7, [sp, #16]
+
+    # num <> 0?
+    MOV r1, #0
+    CMP r0, #0
+    MOVNE r1, #1
+
+    # num <> 1?
+    MOV r2, #0
+    CMP r0, #1
+    MOVNE r2, #1
+
+    # num <> 0 and num <> 1?
+    AND r1, r1, r2
+
+    # If false, num = 0 or 1 -> not prime
+    MOV r3, #0
+    CMP r1, r3
+    BEQ SetNotPrime
+        # Save base array and size
+        LDR r4, =primeArray
+        LDR r5, =primeArraySize
+        LDR r5, [r5]
+
+        MOV r7, r0  // Store num in preserved register
+  
+        # Initialize loop for checking primes
+        # r4 - Array base
+        # r5 - End loop index
+        # r6 - Counter
+        MOV r6, #0
+        StartCheckLoop:
+            # If counter >= array size, end loop
+            CMP r6, r5
+            BGE EndCheckLoop
+
+                # Set up inputs for mod: r0 = num, r1 = prime
+                MOV r0, r7
+                ADD r1, r4, r6, LSL #2
+                LDR r1, [r1, #0]
+
+                # num = prime -> isPrime
+                CMP r0, r1
+                BEQ EndCheckLoop
+                    # Calculate num mod prime
+                    BL modulo
+
+                    # Divisible by prime -> Not prime
+                    CMP r0, #0
+                    BEQ SetNotPrime 
+                        # Increment counter
+                        ADD r6, r6, #1 
+                        B StartCheckLoop
+
+        EndCheckLoop:
+            # No prime factor or is prime factor -> isPrime
+            MOV r0, #1
+            B ExitIsPrime
+    
+    SetNotPrime:
+        # Divisible by prime or is 0, 1 -> not isPrime
+        MOV r0, #0 
+        
+    ExitIsPrime:
+        # Pop stack and exit
+        LDR lr, [sp, #0]
+        LDR r4, [sp, #4]
+        LDR r5, [sp, #8]
+        LDR r6, [sp, #12]
+        LDR r7, [sp, #16]
+        ADD sp, sp, #20
+        MOV pc, lr
 
 .data
    primeArray:  .word 2
