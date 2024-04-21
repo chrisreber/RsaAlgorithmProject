f2f14444afaa0419be00bdd025a938ab73a7e8ae
chrisreber <chris.reber@jhuapl.edu>
2024-04-21 17:37:08 -0400

    Un-commented Andrea's test code

--- RSA.s
+++ RSA.s
@@ -38,35 +38,35 @@ main:
     BL printf
     # -----End Chris testing-----
 
-    @ # Prompt for and receive input for p and q
-    @ # Contributor: Andrea Henry
+    # Prompt for and receive input for p and q
+    # Contributor: Andrea Henry
    
-    @ # Call function to get p
-    @ MOV r0, #1  // input parameter = 1 for p
-    @ BL getPrime
-    @ MOV r4, r0  // Save p
+    # Call function to get p
+    MOV r0, #1  // input parameter = 1 for p
+    BL getPrime
+    MOV r4, r0  // Save p
 
-    @ # Call function to get q
-    @ MOV r0, #2  // input parameter = 2 for q
-    @ BL getPrime
-    @ MOV r5, r0  // Save q
+    # Call function to get q
+    MOV r0, #2  // input parameter = 2 for q
+    BL getPrime
+    MOV r5, r0  // Save q
   
-    @ # Calculate modulus n for public and private keys
-    @ # Contributor: Andrea Henry
+    # Calculate modulus n for public and private keys
+    # Contributor: Andrea Henry
 
-    @ MUL r6, r4, r5  // r6 = n = pq
+    MUL r6, r4, r5  // r6 = n = pq
 
-    @ # Calculate totient, T = (p-1)(q-1)
-    @ # Contributor: Andrea Henry
+    # Calculate totient, T = (p-1)(q-1)
+    # Contributor: Andrea Henry
 
-    @ SUB r4, r4, #1  // p-1
-    @ SUB r5, r5, #1  // q-1
-    @ MUL r7, r4, r5
+    SUB r4, r4, #1  // p-1
+    SUB r5, r5, #1  // q-1
+    MUL r7, r4, r5
         
-    @ # FOR TESTING: Display totient 
-    @ LDR r0, =testTotient
-    @ MOV r1, r7
-    @ BL printf
+    # FOR TESTING: Display totient 
+    LDR r0, =testTotient
+    MOV r1, r7
+    BL printf
 
   # For future operations, T is in r7. Other regs can be overwritten
   
