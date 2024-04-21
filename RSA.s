678395a616f3863d0e474be281c9cf04d3cc3c25
chrisreber <chris.reber@jhuapl.edu>
2024-04-21 16:53:01 -0400

    Added test for modulo function

--- RSA.s
+++ RSA.s
@@ -6,35 +6,52 @@ main:
     SUB sp, sp, #4
     STR lr, [sp, #0]
 
-    # Prompt for and receive input for p and q
-    # Contributor: Andrea Henry
-   
-    # Call function to get p
-    MOV r0, #1  // input parameter = 1 for p
-    BL getPrime
-    MOV r4, r0  // Save p
-
-    # Call function to get q
-    MOV r0, #2  // input parameter = 2 for q
-    BL getPrime
-    MOV r5, r0  // Save q
-  
-    # Calculate modulus n for public and private keys
-    # Contributor: Andrea Henry
-
-    MUL r6, r4, r5  // r6 = n = pq
-
-    # Calculate totient, T = (p-1)(q-1)
-    # Contributor: Andrea Henry
-
-    SUB r4, r4, #1  // p-1
-    SUB r5, r5, #1  // q-1
-    MUL r7, r4, r5
-        
-    # FOR TESTING: Display totient 
-    LDR r0, =testTotient
-    MOV r1, r7
+    # -----Chris testing-----
+    # double check modulo function
+    MOV r0, #4
+    MOV r1, #3
+    BL modulo
+    MOV r1, r0
+    LDR r0, =moduloTest
     BL printf
+
+    MOV r0, #4
+    MOV r1, #1
+    BL modulo
+    MOV r1, r0
+    LDR r0, =moduloTest
+    BL printf
+    # -----End Chris testing-----
+
+    @ # Prompt for and receive input for p and q
+    @ # Contributor: Andrea Henry
+   
+    @ # Call function to get p
+    @ MOV r0, #1  // input parameter = 1 for p
+    @ BL getPrime
+    @ MOV r4, r0  // Save p
+
+    @ # Call function to get q
+    @ MOV r0, #2  // input parameter = 2 for q
+    @ BL getPrime
+    @ MOV r5, r0  // Save q
+  
+    @ # Calculate modulus n for public and private keys
+    @ # Contributor: Andrea Henry
+
+    @ MUL r6, r4, r5  // r6 = n = pq
+
+    @ # Calculate totient, T = (p-1)(q-1)
+    @ # Contributor: Andrea Henry
+
+    @ SUB r4, r4, #1  // p-1
+    @ SUB r5, r5, #1  // q-1
+    @ MUL r7, r4, r5
+        
+    @ # FOR TESTING: Display totient 
+    @ LDR r0, =testTotient
+    @ MOV r1, r7
+    @ BL printf
 
   # For future operations, T is in r7. Other regs can be overwritten
   
@@ -47,4 +64,4 @@ main:
 .data
     # For testing
     testTotient: .asciz "Totient is: %d\n\n"
-
+    moduloTest:  .asciz "Modulo is: %d\n"
